281c09de7ffa5211c779c3eb55d72dd774b60977
Marcelo Barreto <marcelobarretojunior@gmail.com>
2021-04-19 12:54:41 -0300

    first commit

---
+++ spec/via_cep_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe ViaCep do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/via_cep.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `ViaCep`
+module ViaCep
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
