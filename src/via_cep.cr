f95974c3fb12fce91c28428d7183e2f68f609c73
Marcelo Barreto <marcelobarretojunior@gmail.com>
2021-04-19 13:01:43 -0300

    add first working example

--- src/via_cep.cr
+++ src/via_cep.cr
@@ -1,6 +1,14 @@
+require "http/client"
+
 # TODO: Write documentation for `ViaCep`
 module ViaCep
   VERSION = "0.1.0"
+  BASE_URL = "http://viacep.com.br/ws"
 
-  # TODO: Put your code here
+  def self.zipcode(zipcode : String)
+    url = "#{BASE_URL}/#{zipcode}/json"
+    response = HTTP::Client.get(url)
+  end
 end
+
+ViaCep.zipcode("01001000")
