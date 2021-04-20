4d1fc9ee6acf4b5355fcef6b7d606f6492498fb3
Marcelo Barreto <marcelobarretojunior@gmail.com>
2021-04-19 22:08:41 -0300

    feat: finish first working example

--- src/via_cep.cr
+++ src/via_cep.cr
@@ -1,14 +1,10 @@
 require "http/client"
+require "json"
+
+require "./via_cep/zipcode"
 
 # TODO: Write documentation for `ViaCep`
 module ViaCep
   VERSION = "0.1.0"
   BASE_URL = "http://viacep.com.br/ws"
-
-  def self.zipcode(zipcode : String)
-    url = "#{BASE_URL}/#{zipcode}/json"
-    response = HTTP::Client.get(url)
-  end
 end
-
-ViaCep.zipcode("01001000")
---
+++ src/via_cep/zipcode.cr
@@ -0,0 +1,58 @@
+module ViaCep
+  class Zipcode
+    getter zipcode : String
+    getter street : (String)?
+    getter complement : (String)?
+    getter neighborhood : (String)?
+    getter city : (String)?
+    getter state : (String)?
+    getter ibge : (String)?
+    getter gia : (String)?
+    getter ddd : (String)?
+    getter siafi : (String)?
+
+    def initialize(zipcode : String)
+      @zipcode = zipcode
+    end
+
+    def valid?
+      # TODO
+    end
+
+    def call
+      request = HTTP::Client.get("#{BASE_URL}/#{zipcode}/json")
+      response = Hash(String, String).from_json(request.body)
+      set_values(response)
+
+      self
+    end
+
+    private def set_values(values : Hash)
+      @zipcode = values["cep"]
+      @street = values["logradouro"]
+      @complement = values["complemento"]
+      @neighborhood = values["bairro"]
+      @city = values["localidade"]
+      @state = values["uf"]
+      @ibge = values["ibge"]
+      @gia = values["gia"]
+      @ddd = values["ddd"]
+      @siafi = values["siafi"]
+    end
+
+    def to_json
+      {
+        zipcode: zipcode,
+        street: street,
+        complement: complement,
+        neighborhood: neighborhood,
+        city: city,
+        state: state,
+        ibge: ibge,
+        gia: gia,
+        ddd: ddd,
+        siafi: siafi,
+      }
+    end
+  end
+end
